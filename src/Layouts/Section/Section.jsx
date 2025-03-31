90b7d6888f18276754ba05688ee2a0e46912c499
David Steven T <davidtoro013125@gmail,com>
2025-03-31 15:37:48 -0500

    delete margins h2 section 1

--- src/Layouts/Section/Section.jsx
+++ src/Layouts/Section/Section.jsx
@@ -4,7 +4,7 @@ import './Section.css'
 export const Section = ({style}) => {
    return (
       <section className={style}>
-         <h2>Create Task</h2>
+         <h2 id='h2Section1'>Create Task</h2>
          <input className='inputs' type="text" placeholder='Title...' />
          <input className='inputs' id='description' type="text" placeholder='Description...' />
          <input className='btnAdd' type="button" value="Add" />
