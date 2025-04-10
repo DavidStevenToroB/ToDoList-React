57566fc4d4f0dd2e9efc6e3aa92b497d860606d1
David Steven T <davidtoro013125@gmail,com>
2025-04-10 15:14:58 -0500

    view Tasks

--- src/Context/Context.jsx
+++ src/Context/Context.jsx
@@ -2,3 +2,13 @@ import { createContext } from "react";
 
 export const taskContext = createContext()
 
+const MyProvider = ({ children }) => {
+   const [state, setState] = useState('Hola, Mundo');
+ 
+   return (
+     <taskContext.Provider value={{ state, setState }}>
+       {children}
+     </taskContext.Provider>
+   );
+ };
+ 
--- src/Layouts/DivCards/DivCards.jsx
+++ src/Layouts/DivCards/DivCards.jsx
@@ -1,52 +1,29 @@
-import React from 'react'
-const tasks = [
-   {"title": "newTarea", "description": "esta es una tarea de prueva" },
-   {"title": "newTarea2", "description": "esta es una tarea de prueva2" }
+import React, { useState } from 'react'
+import './DivCards.css'
+
+const saveTasks = [
+   { "title": "newTarea", "description": "esta es una tarea de prueva" },
+   { "title": "newTarea2", "description": "esta es una tarea de prueva2" },
+   { "title": "newTarea2", "description": "esta es una tarea de prueva2" },
+   { "title": "newTarea2", "description": "esta es una tarea de prueva2" }
 ]
 
-const createCard = (task) => {
-   const card = document.getElementById('card')
+export const DivCards = () => {
 
+   const [tasks, setTasks] = useState(saveTasks)
 
-   const  inputCheckBox = document.createElement('input')
-   inputCheckBox.type = 'checkbox'
-   inputCheckBox.className = 'inputCheckBox'
-
-   const titleCard = document.createElement('h3')
-   titleCard.textContent = task.title
-
-   const descriptionCard = document.createElement('h4')
-   descriptionCard.textContent = task.description
-
-
-      card.appendChild(inputCheckBox)
-      card.appendChild(titleCard)
-      card.appendChild(descriptionCard)
-}
-
-const createTask = (title, description) => {
-   let newTask = {
-      "title": title, "description": description
-   }
-
-   tasks.push(newTask)
-}
-
-export const DivCards = ({contentTitle, contentDescription}) => {
-   
-   createTask({contentTitle, contentDescription})
-   tasks.forEach(task => 
-      createCard(task)
-   )
-   
 
    return (
       <div id="card">
-         <input className='inputCheckBox' type="checkbox" />
-         <h3>Title</h3>
-         <h4>Description</h4>
+         {tasks.map((task, index) => (
+            <div key={index} className="task-card">
+               <input className="inputCheckBox" type="checkbox" />
+               <h3>{task.title}</h3>
+               <h4>{task.description}</h4>
+            </div>
+         ))}
       </div>
    )
 }
 
- 
+
